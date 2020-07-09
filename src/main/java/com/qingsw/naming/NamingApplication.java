fa34880663303702f51436dd1ca5f0fd54ba46d0
Wang <harry.wang@sap.com>
2020-07-09 20:48:23 +0800

    init

---
+++ src/main/java/com/qingsw/naming/NamingApplication.java
@@ -0,0 +1,13 @@
+package com.qingsw.naming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NamingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NamingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qingsw/naming/NamingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qingsw.naming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
